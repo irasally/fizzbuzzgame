8543d263dc4e01ce42d708beb08ab718580566d2
irasally <irasally@gmail.com>
2013-11-30 13:59:02 +0900

    Gameモデルを作る

--- js/fizzbuzz_app.js
+++ js/fizzbuzz_app.js
@@ -1,2 +1,12 @@
 (function(){
+  var Game = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        number: this.getRamdom
+      };
+    },
+    getRandom: function(){
+      return 5; // rand
+    }
+  });
 })();
