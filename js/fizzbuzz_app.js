01e052223039a2df271296cc1c4cf01c37292fe4
irasally <irasally@gmail.com>
2013-11-30 14:09:41 +0900

    Fizzボタンのイベントを拾うサンプル実装

--- js/fizzbuzz_app.js
+++ js/fizzbuzz_app.js
@@ -2,11 +2,26 @@
   var Game = Backbone.Model.extend({
     defaults: function() {
       return {
-        number: this.getRamdom
+        number: this.getRandom()
       };
     },
     getRandom: function(){
       return 5; // rand
     }
   });
+
+  var GameView = Backbone.View.extend({
+    el: $('#game'),
+    events: {
+      "click #fizz" : "isFizz"
+    },
+    isFizz: function(){
+      var num = this.model.get('number');
+      alert("Fizzを押しました。現在の数字は" + num + "です。");
+    }
+  });
+
+  var game = new Game();
+  console.log(game.get('number'));
+  var gameView = new GameView({model: game});
 })();
