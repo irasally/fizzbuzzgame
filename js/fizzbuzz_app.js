ba82bf62d6452a2bdb90ef69759da6025c893230
irasally <irasally@gmail.com>
2013-11-30 15:44:29 +0900

    結果オブジェクトを表示する場所を変える

--- js/fizzbuzz_app.js
+++ js/fizzbuzz_app.js
@@ -32,7 +32,6 @@
     render: function(){
       var template = this.template(this.model.toJSON());
       this.$el.html(template);
-      console.log(this.$el);
       return this;
     }
   });
@@ -70,7 +69,7 @@
       var result = new Result({message: message});
       console.log(message);
       var resultView = new ResultView({model: result});
-      this.$el.append(resultView.render().el);
+      this.$el.children('#result').html(resultView.render().el);
     }
   });
 
